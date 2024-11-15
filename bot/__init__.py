11be1fd4dc2a8d5eff46e06bc6fbbba545b50f96
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-11-15 19:12:52 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -52,8 +52,8 @@ class Var:
     FFCODE_1080 = getenv("FFCODE_1080") or """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1920x1080 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_720 = getenv("FFCODE_720") or """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 1280x720 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_480 = getenv("FFCODE_480") or """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 854x480 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
-    FFCODE_360 = getenv("FFCODE_360") or """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 640x360 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
-    FFCODE_361 = getenv("FFCODE_361") or """ffmpeg -i '{}' -progress '{}' -c:v libaom-av1 -crf 30 -pix_fmt yuv420p -s 640x360 -b:v 150k -c:a libopus -b:a 35k -preset veryfast -metadata title='𝖳𝖦>@Arctix' -metadata author='𝖳𝖦>@Arctix' '{}' -y"""
+    FFCODE_361 = getenv("FFCODE_360") or """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 640x360 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
+    FFCODE_360 = getenv("FFCODE_361") or """ffmpeg -i '{}' -progress '{}' -c:v libaom-av1 -crf 30 -pix_fmt yuv420p -s 640x360 -b:v 150k -c:a libopus -b:a 35k -preset superfast -metadata title='𝖳𝖦>@Arctix' -metadata author='𝖳𝖦>@Arctix' '{}' -y"""
     QUALS = getenv("QUALS", "360 480 720 1080").split()
     
     AS_DOC = getenv("AS_DOC", "True").lower() == "true"
