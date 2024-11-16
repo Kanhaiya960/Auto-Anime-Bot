1f174f18b5d1d3b86540a562b75306dc69d3f0e2
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-11-16 18:47:10 +0530

    Update ffencoder.py

--- bot/core/ffencoder.py
+++ bot/core/ffencoder.py
@@ -63,7 +63,7 @@ class FFEncoder:
     ‣ <b>Time Took :</b> {convertTime(diff)}
     ‣ <b>Time Left :</b> {convertTime(eta)}</blockquote>"""
                 cancel_markup = InlineKeyboardMarkup([
-                    [InlineKeyboardButton("Cancel Encoding", callback_data=f"cancel_encoding:{__encodeid}")]
+                    [InlineKeyboardButton("Cancel Encoding", callback_data=f"cancel_encoding:{self.__encodeid}")]
                 ])
                 await editMessage(self.message, progress_str, buttons=cancel_markup)
                 if (prog := findall(r"progress=(\w+)", text)) and prog[-1] == 'end':
