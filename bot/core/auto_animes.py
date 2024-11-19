bdfff560eef10adb5f0b276b4002bdeba31532c9
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-11-19 23:05:56 +0530

    Update auto_animes.py

--- bot/core/auto_animes.py
+++ bot/core/auto_animes.py
@@ -1,365 +1,124 @@
+
+import asyncio
+from io import BytesIO
+import io
+import logging
 import os
-import time
-from moviepy.editor import VideoFileClip
-from PIL import Image
-from asyncio import gather, create_task, sleep as asleep, Event
-from asyncio.subprocess import PIPE
-from os import path as ospath, system
-from aiofiles import open as aiopen
-from aiofiles.os import remove as aioremove
-from traceback import format_exc
-from base64 import urlsafe_b64encode
-#from time import time
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+import sys
+import traceback
+from bot import bot as Drone
+from pyrogram import Client, filters
 
-from bot import bot, bot_loop, Var, ani_cache, ffQueue, ffLock, ff_queued
-from .tordownload import TorDownloader
-from .database import db
-from .func_utils import getfeed, encode, editMessage, sendMessage, convertBytes
-from .text_utils import TextEditor
-from .ffencoder import FFEncoder
-from .tguploader import TgUploader
-from .reporter import rep
-from .utils import progress_for_pyrogram
-
-btn_formatter = {
-    '1080':'𝟭𝟬𝟴𝟬𝗽', 
-    '720':'𝟳𝟮𝟬𝗽',
-    '480':'𝟰𝟴𝟬𝗽',
-    '360':'𝟯𝟲𝟬𝗽'
-}
-
-ff_encoders = {}
-file_path_cache = {}
-
-async def download_thumbnail(video, thumbnail_path="thumbnail.jpg"):
+@Drone.on_message(filters.command(['exec']))
+async def execution(bot, message):
+    status_message = await message.reply_text("Processing ...")
+    # DELAY_BETWEEN_EDITS = 0.3
+    # PROCESS_RUN_TIME = 100
     try:
-        clip = VideoFileClip(video)
-        duration = clip.duration
-        thumbnail_time = duration / 2
-        frame = clip.get_frame(thumbnail_time)
-        image = Image.fromarray(frame)
-        image.save(thumbnail_path)
-        clip.close()
-        return thumbnail_path 
-    except Exception as e:
-        print(f"Error generating thumbnail: {e}")
-        return None
-
-def get_video_info(video_path):
-    try:
-        clip = VideoFileClip(video_path)
-        duration = clip.duration  # Duration in seconds
-        width, height = clip.size  # Video resolution
-        clip.close()
-        print("Video information retrieved successfully!")
-        return duration, width, height
-    except Exception as e:
-        print(f"Error getting video info: {e}")
-        return None, None, None
-        
-async def fetch_animes():
-    await rep.report("Fetch Animes Started !!", "info")
-    while True:
-        await asleep(60)
-        if ani_cache['fetch_animes']:
-            for link in Var.RSS_ITEMS:
-                if (info := await getfeed(link, 0)):
-                    bot_loop.create_task(get_animes(info.title, info.link))
-
-@bot.on_callback_query()
-async def callback_handler(client, query: CallbackQuery):
-    if query.data.startswith("queue_status:"):
-        encodeid = int(query.data.split(":")[1])
-        position = list(ffQueue._queue).index(encodeid) + 1
-        total_tasks = ffQueue.qsize()
-        await query.answer(
-            f"Queue Position: {position}\nTotal Queue: {total_tasks}",
-            show_alert=True
-        )
-
-    elif query.data.startswith("remove_task:"):
-        data = query.data.split(":")
-        encodeid = int(data[1])  # The encodeid
-        fpath = file_path_cache.get(encodeid)  # Retrieve the file path from cache
-        
-        if fpath:
-            # Proceed with the removal of the task and file
-            temp_queue = []
-            removed = False
-            while not ffQueue.empty():
-                task = await ffQueue.get()
-                if task == encodeid:
-                    removed = True  # Mark task as removed
-                    continue  # Skip this task
-                temp_queue.append(task)
-
-            # Re-add the remaining tasks back to the queue
-            for task in temp_queue:
-                await ffQueue.put(task)
-
-            # Delete the file associated with the task
-            if removed and os.path.exists(fpath):
-                try:
-                    await aioremove(fpath)  # Remove the file
-                    await query.answer("Task removed from the queue and file deleted.", show_alert=True)
-                except Exception as e:
-                    await query.answer(f"Error deleting file: {e}", show_alert=True)
-            elif removed:
-                await query.answer("Task removed, but file not found.", show_alert=True)
-            
-            # Remove from the cache after the task is processed
-            file_path_cache.pop(encodeid, None)
-
-            # Delete the queue status message
-            await query.message.delete()
-        else:
-            await query.answer("File path not found in cache.", show_alert=True)
-    
-    elif query.data.startswith("cancel_encoding:"):
-        # Extract the file name (encoded filename)
-        encodeid = int(query.data.split(":")[1])
-
-        # Check if the encoding task is in progress and cancel it
-        encodeid = int(query.data.split(":")[1])
-
-        # Check if the encoding task exists
-        if encodeid in ff_encoders:
-            encoder = ff_encoders[encodeid]  # Retrieve the FFEncoder instance
-            await encoder.cancel_encode()  # Call cancel_encode to stop the encoding
-            await query.answer("Encoding process has been canceled.", show_alert=True)
-        else:
-            await query.answer("No encoding task found to cancel.", show_alert=True)
-
-
-async def fencode(fname, fpath, message, m):
-    # Notify the user that encoding has started
-    #t = time.time()
-    encode = await m.edit_text(
-        f"File downloaded successfully:\n\n"
-        f"    • <b>File Name:</b> {fname}\n"
-        f"    • <b>File Path:</b> {fpath}"
-    )
-    stat_msg = await m.edit_text(
-        f"‣ <b>File Name :</b> <b><i>{fname}</i></b>\n\n<i>Processing...</i>",
-    )
-    
-    encodeid = encode.id
-    ffEvent = Event()
-    ff_queued[encodeid] = ffEvent
-
-    # If the lock is already engaged, inform the user that the task is queued
-    if ffLock.locked():
-        file_path_cache[encodeid] = fpath
-        queue_markup = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("Queue Status", callback_data=f"queue_status:{encodeid}")],
-            [InlineKeyboardButton("Remove from Queue", callback_data=f"remove_task:{encodeid}")]
-        ]
-        )
-        await stat_msg.edit_text(
-            f"‣ <b>File Name :</b> <b><i>{fname}</i></b>\n\n<i>Queued to Encode...</i>",
-            reply_markup=queue_markup
-        )
-
-    #encoder = FFEncoder(stat_msg, fpath, fname, encodeid, "360")
-    #ff_encoders[encodeid] = encoder
-    
-    # Add the encoding task to the queue and wait for its turn
-    await ffQueue.put(encodeid)
-    await ffEvent.wait()
- 
-    t = time.time()
-   
-    # Acquire the lock for the current encoding task
-    await ffLock.acquire()
-    await stat_msg.edit_text(
-        f"‣ <b>File Name :</b> <b><i>{fname}</i></b>\n\n<i>Ready to Encode...</i>"
-    )
-
-    await asleep(1.5)
-
-    try:
-        # Start the encoding process
-        out_path = await FFEncoder(stat_msg, fpath, fname, encodeid, "360").start_encode()    
-    except Exception as e:
-        await stat_msg.delete()
-        await aioremove(out_path)
-        await aioremove(fpath)
-        #await encode.delete()
-        ffLock.release()
-        return await message.reply(f"<b>Encoding failed: {str(e)}</b>")
-
-    await stat_msg.edit_text("<b>Successfully Compressed. Now proceeding to upload...</b>")
-    await asleep(1.5)
-
-    try:
-        start_time = time.time()
-        duration, width, height = get_video_info(out_path)
-        thumbnail_path = await download_thumbnail(out_path)
-        
-        # Upload the encoded file using Pyrogram's send_video
-        #await bot.send_document(
-        #    chat_id=message.chat.id,
-        #    document=out_path,
-        #    thumb="thumb.jpg" if ospath.exists("thumb.jpg") else None,                  
-        #    force_document=True,
-        #    caption=f"‣ <b>File Name:</b> <i>{fname}</i>\n‣ <b>Status:</b> Uploaded Successfully.",
-        #    progress=progress_for_pyrogram,
-        #    progress_args=("<b>Upload Started....</b>", stat_msg, start_time)
-        #)
-        msg = await bot.send_video(
-            chat_id=message.chat.id,
-            video=out_path,
-            thumb=thumbnail_path,
-            caption=f"‣ <b>File Name:</b> <i>{fname}</i>",
-            duration=int(duration),
-            width=width,
-            height=height,
-            supports_streaming=True,
-            progress=progress_for_pyrogram,
-            progress_args=("<b>Upload Started....</b>", stat_msg, start_time)
-        )
-        #channel_id = int(-1001825550753)  # Replace with your channel ID
-        #await msg.copy(chat_id=channel_id)
-        channel_ids = [
-            int(-1001825550753),
-            int(-1002373955828)
-        ]
-
-        for channel_id in channel_ids:
-            await msg.copy(chat_id=channel_id)
-    except Exception as e:
-        await message.reply(
-            f"<b>Error during upload: {e}. Encoding task canceled, please retry.</b>"
-        )
-        await stat_msg.delete()
-        await aioremove(out_path)
-        await aioremove(fpath)
-        #await encode.delete()
-        ffLock.release()
+        cmd = message.text.split(" ", maxsplit=1)[1]
+    except:
+        await status_message.edit(text="input not found")
         return
-    finally:
-        await aioremove(out_path)
-        await aioremove(fpath)
-        await aioremove(thumbnail_path)
-
-    # Release the lock once the task is completed
-    ffLock.release()
-    await stat_msg.delete()
-    total_time = time.time() - t
-    formatted_time = time.strftime("%H:%M:%S", time.gmtime(total_time))
-    #await encode.delete()
-    #await message.reply(
-    #    f"‣ <b>File Name :</b> <b><i>{fname}</i></b>\n\n<i>Upload completed successfully.</i>"
-    #)
-    await message.reply(
-        f"‣ <b>File Name:</b> <b><i>{fname}</i></b>\n\n"
-        f"<i>Upload completed successfully.</i>\n"
-        f"‣ <b>Total Time Taken:</b> {formatted_time}"
+    reply_to_ = message
+    # start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
+    stdout, stderr = await process.communicate()
+    e = stderr.decode()
+    if not e:
+        e = "No Error"
+    o = stdout.decode()
+    if not o:
+        o = "No Output"
 
+    OUTPUT = ""
+    OUTPUT += f"<b>QUERY:</b>\n<u>Command:</u>\n<code>{cmd}</code> \n"
+    OUTPUT += f"<u>PID</u>: <code>{process.pid}</code>\n\n"
+    OUTPUT += f"<b>stderr</b>: \n<code>{e}</code>\n\n"
+    OUTPUT += f"<b>stdout</b>: \n<code>{o}</code>"
 
-
-
-async def get_animes(name, torrent, force=False):
-    try:
-        aniInfo = TextEditor(name)
-        await aniInfo.load_anilist()
-        ani_id, ep_no = aniInfo.adata.get('id'), aniInfo.pdata.get("episode_number")
-        if ani_id not in ani_cache['ongoing']:
-            ani_cache['ongoing'].add(ani_id)
-        elif not force:
-            return
-        if not force and ani_id in ani_cache['completed']:
-            return
-        if force or (not (ani_data := await db.getAnime(ani_id)) \
-            or (ani_data and not (qual_data := ani_data.get(ep_no))) \
-            or (ani_data and qual_data and not all(qual for qual in qual_data.values()))):
-            
-            if "[Batch]" in name:
-                await rep.report(f"Torrent Skipped!\n\n{name}", "warning")
-                return
-            
-            await rep.report(f"New Anime Torrent Found!\n\n{name}", "info")
-            post_msg = await bot.send_photo(
-                Var.MAIN_CHANNEL,
-                photo=await aniInfo.get_poster(),
-                caption=await aniInfo.get_caption()
+    if len(OUTPUT) > 4096:
+        with BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "exec.text"
+            await reply_to_.reply_document(
+                document=out_file,
+                caption=cmd,
+                disable_notification=True
             )
-            #post_msg = await sendMessage(Var.MAIN_CHANNEL, (await aniInfo.get_caption()).format(await aniInfo.get_poster()), invert_media=True)
-            
-            await asleep(1.5)
-            stat_msg = await sendMessage(Var.MAIN_CHANNEL, f"‣ <b>Anime Name :</b> <b><i>{name}</i></b>\n\n<i>Downloading...</i>")
-            await rep.report(f"The Torrent Link Was!\n\n{torrent}", "info")
-            dl = await TorDownloader("./downloads").download(torrent, name)
-            if not dl or not ospath.exists(dl):
-                await rep.report(f"File Download Incomplete, Try Again", "error")
-                await stat_msg.delete()
-                return
+    else:
+        await reply_to_.reply_text(OUTPUT)
 
-            post_id = post_msg.id
-            ffEvent = Event()
-            ff_queued[post_id] = ffEvent
-            if ffLock.locked():
-                await editMessage(stat_msg, f"‣ <b>Anime Name :</b> <b><i>{name}</i></b>\n\n<i>Queued to Encode...</i>")
-                await rep.report("Added Task to Queue...", "info")
-            await ffQueue.put(post_id)
-            await ffEvent.wait()
-            
-            await ffLock.acquire()
-            btns = []
-            for qual in Var.QUALS:
-                filename = await aniInfo.get_upname(qual)
-                await editMessage(stat_msg, f"‣ <b>Anime Name :</b> <b><i>{name}</i></b>\n\n<i>Ready to Encode...</i>")
-                
-                await asleep(1.5)
-                await rep.report("Starting Encode...", "info")
-                try:
-                    out_path = await FFEncoder(stat_msg, dl, filename, qual).start_encode()
-                except Exception as e:
-                    await rep.report(f"Error: {e}, Cancelled,  Retry Again !", "error")
-                    await stat_msg.delete()
-                    ffLock.release()
-                    return
-                await rep.report("Succesfully Compressed Now Going To Upload...", "info")
-                
-                await editMessage(stat_msg, f"‣ <b>Anime Name :</b> <b><i>{filename}</i></b>\n\n<i>Ready to Upload...</i>")
-                await asleep(1.5)
-                try:
-                    msg = await TgUploader(stat_msg).upload(out_path, qual)
-                except Exception as e:
-                    await rep.report(f"Error: {e}, Cancelled,  Retry Again !", "error")
-                    await stat_msg.delete()
-                    ffLock.release()
-                    return
-                await rep.report("Succesfully Uploaded File into Tg...", "info")
-                
-                msg_id = msg.id
-                link = f"https://telegram.me/{(await bot.get_me()).username}?start={await encode('get-'+str(msg_id * abs(Var.FILE_STORE)))}"
-                
-                if post_msg:
-                    if len(btns) != 0 and len(btns[-1]) == 1:
-                        btns[-1].insert(1, InlineKeyboardButton(f"{btn_formatter[qual]} - {convertBytes(msg.document.file_size)}", url=link))
-                    else:
-                        btns.append([InlineKeyboardButton(f"{btn_formatter[qual]} - {convertBytes(msg.document.file_size)}", url=link)])
-                    await editMessage(post_msg, post_msg.caption.html if post_msg.caption else "", InlineKeyboardMarkup(btns))
-                    
-                await db.saveAnime(ani_id, ep_no, qual, post_id)
-                bot_loop.create_task(extra_utils(msg_id, out_path))
-            ffLock.release()
-            
-            await stat_msg.delete()
-            await aioremove(dl)
-        ani_cache['completed'].add(ani_id)
-    except Exception as error:
-        await rep.report(format_exc(), "error")
+    await status_message.delete()
+    
+@Drone.on_message(filters.command(['eval']))
+async def eval(client, message):
+    replied = message.reply_to_message
+    status_message = await message.reply_text("Processing ...")
+    if replied and replied.document and replied.document.file_name.endswith(
+                    ('.py', '.txt')):
+        path = await message.reply_to_message.download()
+        with open(path, "r") as jv:
+            cmd = jv.read()
+        try:
+            os.remove(path)
+        except:
+            pass
+    else:
+        try:
+            cmd = message.text.split(" ", maxsplit=1)[1]
+        except:
+            await status_message.edit(text="input not found")
+            return
+    
+    reply_to_ = message
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
 
-async def extra_utils(msg_id, out_path):
-    msg = await bot.get_messages(Var.FILE_STORE, message_ids=msg_id)
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
 
-    if Var.BACKUP_CHANNEL != 0:
-        for chat_id in Var.BACKUP_CHANNEL.split():
-            await msg.copy(int(chat_id))
-            
-    # MediaInfo, ScreenShots, Sample Video ( Add-ons Features )
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "<b>EVAL</b>: "
+    final_output += f"<code>{cmd}</code>\n\n"
+    final_output += "<b>OUTPUT</b>:\n"
+    final_output += f"<code>{evaluation.strip()}</code> \n"
+
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await reply_to_.reply_document(
+                document=out_file,
+                caption=cmd,
+                disable_notification=True
+            )
+    else:
+        await reply_to_.reply_text(final_output)
+    await status_message.delete()
+
+
+async def aexec(code, client, message):
+    exec(
+        'async def __aexec(client, message): ' +
+        ''.join(f'\n {l_}' for l_ in code.split('\n'))
+    )
+    return await locals()['__aexec'](client, message)
